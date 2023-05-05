68f6b9f7f4d9de67de326bd9c127c0ae7813e9f3
김민규 <gyepi7676@gmail.com>
2023-05-05 17:56:24 +0900

    feat: 코드 블럭 구현

--- src/components/moecules/PostContent.tsx
+++ src/components/moecules/PostContent.tsx
@@ -6,9 +6,17 @@ import Image from "next/image"
 import PostContentH1 from "../atoms/PostContentH1"
 import PostContentH2 from "../atoms/PostContentH2"
 import PostContentH3 from "../atoms/PostContentH3"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 type Props = {
   children: string;
+}
+
+type codeProps = {
+  inline: string,
+  className: string,
+  children: React.ReactNode,
 }
 
 const PostBodyBox = styled.article`
@@ -54,7 +62,22 @@ const customComponent = {
     return (
       <Image src={props.src} alt={props.alt} width={300} height={200} />
     )
-  }
+  },
+  code({ inline, className, children, ...props }: codeProps) {
+    const match = /language-(\w+)/.exec(className || "");
+    return !inline && match ? (
+      <SyntaxHighlighter
+        language={match[1]}
+        PreTag="div"
+        {...props}
+        style={materialDark}
+      >
+        {String(children).replace(/\n$/, "")}
+      </SyntaxHighlighter>
+    ) : (
+      <code {...props}>{children}</code>
+    );
+  },
 }
 
 const PostBody = ({ children } : Props) => {

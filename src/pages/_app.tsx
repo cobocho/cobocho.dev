f33cbdfe5a394e303cdc13a27536b2aec525cb22
cobocho <rlaalsrb1111@naver.com>
2023-05-12 22:59:19 +0900

    feat: 캐노니컬 태그 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,26 @@
 import Layout from '@/components/Layout'
+import { DOMAIN } from '@/constants/domain';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const p = router.asPath.slice(1);
+  const canonicalURL = `https://${DOMAIN}${p}`.split("?")[0];
+  
   return (
-    <RecoilRoot>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </RecoilRoot>
+    <>
+      <Head>
+        <link rel="canonical" href={canonicalURL} />
+      </Head>
+      <RecoilRoot>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RecoilRoot>
+    </>
   )
 }

ea2eec5f110419618ce3293eaeb8bea8d67dbe10
cobocho <rlaalsrb1111@naver.com>
2023-05-16 23:14:06 +0900

    fix: scrollObserver 옵션 수정

--- src/lib/getIntersectionObserver.ts
+++ src/lib/getIntersectionObserver.ts
@@ -2,14 +2,13 @@ import { Dispatch, SetStateAction } from 'react';
 
 const observerOption = {
   threshold: 0.4,
-  rootMargin: '-76px 0px 0px 0px',
+  rootMargin: '-60px 0px 0px 0px',
 };
 
 export const getIntersectionObserver = (setState: Dispatch<SetStateAction<string>>) => {
   let direction = '';
   let prevYposition = 0;
 
-  // scroll 방향 check function
   const checkScrollDirection = (prevY: number) => {
     if (window.scrollY === 0 && prevY === 0) return;
     else if (window.scrollY > prevY) direction = 'down';

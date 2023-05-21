c43bc973bca8ddc45428f56bcc78ef91c78c1ae0
cobocho <rlaalsrb1111@naver.com>
2023-05-22 00:26:25 +0900

    docs: `개발자 지망생이 읽은 「함께 자라기 : 애자일로 가는 길」` 작성 완료

--- src/constants/categoryTrans.ts
+++ src/constants/categoryTrans.ts
@@ -6,6 +6,7 @@ export const categoryTrans:
 {
   review: '회고',
   blog: '블로그 제작기',
+  book: '독후감'
 }
 
 export const categoryDescription: 
@@ -17,5 +18,6 @@ export const categoryDescription:
   Recent: '최근 포스트입니다',
   review: '반성과 후회의 장',
   blog: '관상용 블로그 육성일기',
-  edd: 'Error Driven Development'
+  edd: 'Error Driven Development',
+  book: '책책책 책을 읽읍시다'
 }

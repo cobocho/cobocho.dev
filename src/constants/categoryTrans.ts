a01c5e2a9de3a970e2294dca96bb22c930335efb
cobocho <rlaalsrb1111@naver.com>
2023-06-26 21:32:27 +0900

    chore: 구름톤 번역 추가

--- src/constants/categoryTrans.ts
+++ src/constants/categoryTrans.ts
@@ -1,23 +1,19 @@
-export const categoryTrans: 
-{
-  [key: string]: string
-} 
-  = 
-{
+export const categoryTrans: {
+  [key: string]: string;
+} = {
   review: '회고',
   blog: '블로그 제작기',
-  book: '독후감'
-}
+  book: '독후감',
+  '9oormthon': '구름톤',
+};
 
-export const categoryDescription: 
-{
-  [key: string]: string
-} 
-  = 
-{
+export const categoryDescription: {
+  [key: string]: string;
+} = {
   Recent: '최근 포스트입니다',
   review: '반성과 후회의 장',
   blog: '관상용 블로그 육성일기',
   edd: 'Error Driven Development',
-  book: '책책책 책을 읽읍시다'
-}
+  book: '책책책 책을 읽읍시다',
+  '9oormthon': '구름톤 생존기',
+};

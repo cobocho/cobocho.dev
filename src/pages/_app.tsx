12679360ac78dc3cb75e9c0439d433933bb0813a
cobocho <rlaalsrb1111@naver.com>
2023-09-13 22:12:55 +0900

    Chore: 구글 애널리틱스 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,28 +4,14 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { RecoilRoot } from 'recoil';
-import * as gtag from '@/lib/gtag';
 import { Analytics } from '@vercel/analytics/react';
 import Script from 'next/script';
-import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const p = router.asPath.slice(1);
   const DOMAIN = process.env.DOMAIN;
   const canonicalURL = `https://${DOMAIN}${p}`.split('?')[0];
-
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    router.events.on('hashChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-      router.events.off('hashChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
 
   return (
     <>
@@ -40,25 +26,6 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </Layout>
       </RecoilRoot>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Analytics />
     </>
   );

3acb38498b3dc0bd0918e992794e256dd0b7f2de
cobocho <rlaalsrb1111@naver.com>
2023-09-18 23:45:02 +0900

    Feat: 페이지네이션 css 수정 및 화살표 버튼 추가

--- src/components/PageList/PageButton.tsx
+++ src/components/PageList/PageButton.tsx
@@ -1,57 +1,73 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import styled from "styled-components";
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
 
-type Props = {
+interface Props {
   pageNumber: number;
 }
 
-const PageButtonBox = styled.li<{ className: string }>`
+const PageButton = ({ pageNumber }: Props) => {
+  const router = useRouter();
+  const { page } = router.query;
+
+  const isCurrentPage = page === String(pageNumber) || (!page && pageNumber === 1);
+
+  const url = router.asPath.split('/');
+
+  if (page) {
+    url[url.length - 1] = String(pageNumber);
+  }
+
+  const hrefLink = page ? url.join('/') : `/${pageNumber}`;
+
+  return (
+    <Container className={isCurrentPage ? 'current-page' : ''}>
+      <Link href={hrefLink}>{pageNumber}</Link>
+    </Container>
+  );
+};
+
+const Container = styled.li<{ className: string }>`
   width: 40px;
   height: 40px;
-  border-radius: 6px;
-  font-size: 20px;
-  background-color: ${props => props.theme.tagColor};
-  opacity: 0.5;
+
+  border-radius: 50%;
+
+  opacity: 0.2;
   transition: all 0.5s;
 
   &.current-page {
     opacity: 1;
-  }
+    background-color: ${(props) => props.theme.textColor};
 
-  &:hover {
-    transform: translateY(-10px);
-    opacity: 0.7;
+    a {
+      color: ${(props) => props.theme.bgColor};
+    }
   }
 
   &.current-page:hover {
     transform: translateY(0);
+    opacity: 1;
+  }
+
+  &:hover {
+    transform: translateY(-4px);
+    opacity: 0.7;
   }
 
   a {
     display: flex;
     justify-content: center;
     align-items: center;
+
     width: 100%;
     height: 100%;
-    font-weight: 800;
-    color: ${props => props.theme.tagTextColor};
+
+    font-family: sans-serif;
+    font-weight: 500;
+    font-size: 20px;
+    color: ${(props) => props.theme.textColor};
   }
-`
-
-const PageButton = ({ pageNumber } : Props) => {
-  const router = useRouter();
-  const { category, page } = router.query;
-  const isCurrentPage = page === String(pageNumber) || (!page && pageNumber === 1);
-
-  const hrefLink = category ? `/category/${category}/${pageNumber}` : `/${pageNumber}`
-  return (
-    <PageButtonBox className={isCurrentPage ? 'current-page' : ''}>
-      <Link href={hrefLink}>
-        {pageNumber}
-      </Link>
-    </PageButtonBox>
-  )
-}
+`;
 
 export default PageButton;
--- src/components/PageList/PageList.tsx
+++ src/components/PageList/PageList.tsx
@@ -1,17 +1,88 @@
 import styled from 'styled-components';
 import PageButton from './PageButton';
+import { useRouter } from 'next/router';
 
-type Props = {
+interface Props {
   postQuantity: number;
+}
+
+const PageList = ({ postQuantity }: Props) => {
+  const router = useRouter();
+  const { page } = router.query;
+
+  const pages = Array.from({ length: Math.ceil(postQuantity / 10) }, (v, i) => i + 1);
+
+  const isFirstPage = page === '1' || !page;
+  const isLastPage = page ? +page === pages.length : pages.length === 1;
+
+  const pageIncreaseHandler = () => {
+    const { query } = router;
+
+    if (!query.page) {
+      router.push('/2');
+      return;
+    }
+
+    const url = router.asPath.split('/');
+    const nextPage = +query.page + 1;
+    url[url.length - 1] = String(nextPage);
+
+    router.push(url.join('/'));
+  };
+
+  const pageDecreaseHandler = () => {
+    const { query } = router;
+
+    if (!query.page) {
+      router.push(`/${pages.length - 1}`);
+      return;
+    }
+
+    const url = router.asPath.split('/');
+    const prevPage = +query.page - 1;
+    url[url.length - 1] = String(prevPage);
+
+    router.push(url.join('/'));
+  };
+
+  return (
+    <Container>
+      <ul>
+        <ArrowButton
+          onClick={pageDecreaseHandler}
+          disabled={isFirstPage}
+        >
+          {'<'}
+        </ArrowButton>
+        {pages.map((page) => {
+          return (
+            <PageButton
+              pageNumber={page}
+              key={page}
+            />
+          );
+        })}
+        <ArrowButton
+          onClick={pageIncreaseHandler}
+          disabled={isLastPage}
+        >
+          {'>'}
+        </ArrowButton>
+      </ul>
+    </Container>
+  );
 };
 
-const PageListBox = styled.div`
+const Container = styled.div`
   display: flex;
   width: 100%;
   height: 30px;
 
   ul {
+    width: fit-content;
     display: flex;
+    align-items: center;
+
     margin: 0 auto;
   }
 
@@ -24,22 +95,20 @@ const PageListBox = styled.div`
   }
 `;
 
-const PageList = ({ postQuantity }: Props) => {
-  const pages = Array.from({ length: Math.ceil(postQuantity / 10) }, (v, i) => i + 1);
-  return (
-    <PageListBox>
-      <ul>
-        {pages.map((page) => {
-          return (
-            <PageButton
-              pageNumber={page}
-              key={page}
-            />
-          );
-        })}
-      </ul>
-    </PageListBox>
-  );
-};
+const ArrowButton = styled.button`
+  width: 40px;
+  height: 40px;
+
+  margin: 0 20px;
+
+  border: none;
+  background-color: transparent;
+
+  font-weight: 800;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export default PageList;

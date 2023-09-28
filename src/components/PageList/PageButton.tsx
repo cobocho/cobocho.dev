0b27f76150d4e6601d58da952ad72d889e2a38df
cobocho <rlaalsrb1111@naver.com>
2023-09-28 23:40:50 +0900

    Refactor: 페이지네이션 훅 분리

--- src/components/PageList/PageButton.tsx
+++ src/components/PageList/PageButton.tsx
@@ -1,73 +1,73 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
+import usePagination from './usePagination';
 
 interface Props {
-  pageNumber: number;
+	pageNumber: number;
+	postQuantity: number;
 }
 
-const PageButton = ({ pageNumber }: Props) => {
-  const router = useRouter();
-  const { page } = router.query;
+const getComputedUrl = (url: string, pageNumber: number, page?: string) => {
+	let result = url.split('/');
+	if (page) {
+		result[result.length - 1] = String(pageNumber);
+	}
+	return page ? result.join('/') : `/${pageNumber}`;
+};
 
-  const isCurrentPage = page === String(pageNumber) || (!page && pageNumber === 1);
+const PageButton = ({ pageNumber, postQuantity }: Props) => {
+	const { page, url, isCurrentPage } = usePagination(postQuantity);
+	const hrefLink = getComputedUrl(url, pageNumber, page as string);
 
-  const url = router.asPath.split('/');
-
-  if (page) {
-    url[url.length - 1] = String(pageNumber);
-  }
-
-  const hrefLink = page ? url.join('/') : `/${pageNumber}`;
-
-  return (
-    <Container className={isCurrentPage ? 'current-page' : ''}>
-      <Link href={hrefLink}>{pageNumber}</Link>
-    </Container>
-  );
+	return (
+		<Container className={isCurrentPage(pageNumber) ? 'current-page' : ''}>
+			<Link href={hrefLink}>{pageNumber}</Link>
+		</Container>
+	);
 };
 
 const Container = styled.li<{ className: string }>`
-  width: 40px;
-  height: 40px;
+	width: 40px;
+	height: 40px;
 
-  border-radius: 50%;
+	border-radius: 50%;
 
-  opacity: 0.2;
-  transition: all 0.5s;
+	opacity: 0.2;
+	transition: all 0.5s;
 
-  &.current-page {
-    opacity: 1;
-    background-color: ${(props) => props.theme.textColor};
+	&.current-page {
+		opacity: 1;
+		background-color: ${(props) => props.theme.textColor};
 
-    a {
-      color: ${(props) => props.theme.bgColor};
-    }
-  }
+		a {
+			color: ${(props) => props.theme.bgColor};
+		}
+	}
 
-  &.current-page:hover {
-    transform: translateY(0);
-    opacity: 1;
-  }
+	&.current-page:hover {
+		transform: translateY(0);
+		opacity: 1;
+	}
 
-  &:hover {
-    transform: translateY(-4px);
-    opacity: 0.7;
-  }
+	&:hover {
+		transform: translateY(-4px);
+		opacity: 0.7;
+	}
 
-  a {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+	a {
+		display: flex;
+		justify-content: center;
+		align-items: center;
 
-    width: 100%;
-    height: 100%;
+		width: 100%;
+		height: 100%;
 
-    font-family: sans-serif;
-    font-weight: 500;
-    font-size: 20px;
-    color: ${(props) => props.theme.textColor};
-  }
+		font-family: sans-serif;
+		font-weight: 500;
+		font-size: 20px;
+		color: ${(props) => props.theme.textColor};
+	}
 `;
 
 export default PageButton;
--- src/components/PageList/PageList.tsx
+++ src/components/PageList/PageList.tsx
@@ -1,121 +1,89 @@
 import styled from 'styled-components';
 import PageButton from './PageButton';
 import { useRouter } from 'next/router';
+import usePagination from './usePagination';
 
 interface Props {
-  postQuantity: number;
+	postQuantity: number;
 }
 
 const PageList = ({ postQuantity }: Props) => {
-  const router = useRouter();
-  const { page } = router.query;
+	const { pages, isFirstPage, isLastPage, increasePage, decreasePage } = usePagination(
+		postQuantity || 0
+	);
 
-  const pages = Array.from({ length: Math.ceil(postQuantity / 10) }, (v, i) => i + 1);
-
-  const isFirstPage = page === '1' || !page;
-  const isLastPage = page ? +page === pages.length : pages.length === 1;
-
-  const pageIncreaseHandler = () => {
-    const { query } = router;
-
-    if (!query.page) {
-      router.push('/2');
-      return;
-    }
-
-    const url = router.asPath.split('/');
-    const nextPage = +query.page + 1;
-    url[url.length - 1] = String(nextPage);
-
-    router.push(url.join('/'));
-  };
-
-  const pageDecreaseHandler = () => {
-    const { query } = router;
-
-    if (!query.page) {
-      router.push(`/${pages.length - 1}`);
-      return;
-    }
-
-    const url = router.asPath.split('/');
-    const prevPage = +query.page - 1;
-    url[url.length - 1] = String(prevPage);
-
-    router.push(url.join('/'));
-  };
-
-  return (
-    <Container>
-      <ul>
-        <ArrowButton
-          onClick={pageDecreaseHandler}
-          disabled={isFirstPage}
-        >
-          {'<'}
-        </ArrowButton>
-        {pages.map((page) => {
-          return (
-            <PageButton
-              pageNumber={page}
-              key={page}
-            />
-          );
-        })}
-        <ArrowButton
-          onClick={pageIncreaseHandler}
-          disabled={isLastPage}
-        >
-          {'>'}
-        </ArrowButton>
-      </ul>
-    </Container>
-  );
+	return (
+		<Container>
+			<ul>
+				<ArrowButton
+					onClick={decreasePage}
+					disabled={isFirstPage}
+				>
+					{'<'}
+				</ArrowButton>
+				{pages.map((page) => {
+					return (
+						<PageButton
+							pageNumber={page}
+							postQuantity={postQuantity}
+							key={page}
+						/>
+					);
+				})}
+				<ArrowButton
+					onClick={increasePage}
+					disabled={isLastPage}
+				>
+					{'>'}
+				</ArrowButton>
+			</ul>
+		</Container>
+	);
 };
 
 const Container = styled.div`
-  display: flex;
-  width: 100%;
-  height: 30px;
+	display: flex;
+	width: 100%;
+	height: 30px;
 
-  ul {
-    width: fit-content;
-    display: flex;
-    align-items: center;
+	ul {
+		width: fit-content;
+		display: flex;
+		align-items: center;
 
-    margin: 0 auto;
-  }
+		margin: 0 auto;
+	}
 
-  li {
-    margin-right: 10px;
-  }
+	li {
+		margin-right: 10px;
+	}
 
-  li:last-child {
-    margin-right: 0;
-  }
+	li:last-child {
+		margin-right: 0;
+	}
 `;
 
 const ArrowButton = styled.button`
-  width: 40px;
-  height: 40px;
+	width: 40px;
+	height: 40px;
 
-  margin: 0 20px;
+	margin: 0 20px;
 
-  border: none;
-  background-color: transparent;
+	border: none;
+	background-color: transparent;
 
-  font-weight: 800;
-  color: ${(props) => props.theme.textColor};
+	font-weight: 800;
+	color: ${(props) => props.theme.textColor};
 
-  opacity: 0.6;
+	opacity: 0.6;
 
-  &:disabled {
-    opacity: 0.1;
-  }
+	&:disabled {
+		opacity: 0.1;
+	}
 
-  &:hover {
-    cursor: pointer;
-  }
+	&:hover {
+		cursor: pointer;
+	}
 `;
 
 export default PageList;

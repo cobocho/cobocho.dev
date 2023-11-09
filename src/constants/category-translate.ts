0e3741435fbdca366260ad24b9c872826f10d672
cobocho <rlaalsrb1111@naver.com>
2023-11-09 18:25:23 +0900

    feat: 프리코스 관련 영문 카테고리 변환 추가

--- src/constants/category-translate.ts
+++ src/constants/category-translate.ts
@@ -1,21 +1,23 @@
 export const KOR_CATEGORY: {
-  [key: string]: string;
+	[key: string]: string;
 } = {
-  review: '회고',
-  blog: '블로그 제작기',
-  book: '독후감',
-  'nextstep-js': 'NEXTSTEP',
-  '9oormthon': '구름톤',
+	review: '회고',
+	blog: '블로그 제작기',
+	book: '독후감',
+	'nextstep-js': 'NEXTSTEP',
+	'9oormthon': '구름톤',
+	'woowa-tech': '우아한테크코스',
 };
 
 export const CATEGORY_DESCRIPTIONS: {
-  [key: string]: string;
+	[key: string]: string;
 } = {
-  Recent: '최근 포스트입니다',
-  review: '반성과 후회의 장',
-  blog: '관상용 블로그 육성일기',
-  edd: 'Error Driven Development',
-  book: '책책책 책을 읽읍시다',
-  'nextstep-js': '바닐라JS 방황기',
-  '9oormthon': '구름톤 생존기',
+	Recent: '최근 포스트입니다',
+	review: '반성과 후회의 장',
+	blog: '관상용 블로그 육성일기',
+	edd: 'Error Driven Development',
+	book: '책책책 책을 읽읍시다',
+	'nextstep-js': '바닐라JS 방황기',
+	'9oormthon': '구름톤 생존기',
+	'woowa-tech': '우아한테크코스',
 };

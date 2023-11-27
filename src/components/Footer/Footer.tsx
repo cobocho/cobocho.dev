f748eb0c1362a89043074f7e6ee9bcbd0f236c75
cobocho <rlaalsrb1111@naver.com>
2023-11-27 21:45:36 +0900

    refactor: 카피라이트 년도 변수화

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const FooterBox = styled.footer`
-  width: 100vw;
-  height: 80px;
-  position: relative;
-  text-align: center;
-  font-weight: 300;
-  background-color: ${(props) => props.theme.bgColor};
-  opacity: 0.7;
-  transition: all 0.5s;
-`;
-
 const Footer = () => {
-  return <FooterBox>© 2023. Cobocho all rights reserved.</FooterBox>;
+	const currentYear = new Date().getFullYear();
+
+	return <Container>{`© ${currentYear}. Cobocho all rights reserved.`}</Container>;
 };
+
+const Container = styled.footer`
+	position: relative;
+
+	width: 100vw;
+	height: 80px;
+
+	text-align: center;
+	font-weight: 300;
+
+	background-color: ${(props) => props.theme.bgColor};
+
+	opacity: 0.7;
+
+	transition: all 0.5s;
+`;
 
 export default Footer;

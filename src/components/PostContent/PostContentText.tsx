a60146781a98f70e74c5219e20409ea334d1994e
cobocho <rlaalsrb1111@naver.com>
2023-11-29 02:06:23 +0900

    design: 포스트 텍스트 `line-height` 조정

--- src/components/PostContent/PostContentText.tsx
+++ src/components/PostContent/PostContentText.tsx
@@ -1,8 +1,8 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const PostContentText = styled.p`
-  line-height: 1.7;
-  white-space: pre-wrap;
-`
+	line-height: 1.8;
+	white-space: pre-wrap;
+`;
 
 export default PostContentText;

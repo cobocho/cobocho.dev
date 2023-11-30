91d0e07a610b8b828f315af41ba7c19cbc7ace52
cobocho <rlaalsrb1111@naver.com>
2023-11-30 17:12:59 +0900

    chore: 패키지 의존성 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,28 +5,27 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { RecoilRoot } from 'recoil';
 import { Analytics } from '@vercel/analytics/react';
-import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const p = router.asPath.slice(1);
-  const DOMAIN = process.env.DOMAIN;
-  const canonicalURL = `https://${DOMAIN}${p}`.split('?')[0];
+	const router = useRouter();
+	const p = router.asPath.slice(1);
+	const DOMAIN = process.env.DOMAIN;
+	const canonicalURL = `https://${DOMAIN}${p}`.split('?')[0];
 
-  return (
-    <>
-      <Head>
-        <link
-          rel="canonical"
-          href={canonicalURL}
-        />
-      </Head>
-      <RecoilRoot>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </RecoilRoot>
-      <Analytics />
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<link
+					rel="canonical"
+					href={canonicalURL}
+				/>
+			</Head>
+			<RecoilRoot>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</RecoilRoot>
+			<Analytics />
+		</>
+	);
 }

7740e936288b5d006a9255ca89531649f5f7f547
cobocho <rlaalsrb1111@naver.com>
2023-11-30 23:33:41 +0900

    chore: `@types/react-syntax-highlighter` 설치

--- src/components/PostContent/PostContent.tsx
+++ src/components/PostContent/PostContent.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 

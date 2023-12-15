6215ae91b09315774de9b061883a1897fdc63e68
cobocho <rlaalsrb1111@naver.com>
2023-12-15 23:48:44 +0900

    feat: 아이콘 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,20 @@ export default function RootLayout({ children }: PropsWithChildren) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
         />
-        <meta name="naver-site-verification" content="04d43d4b82d52d0217962b5a9d60fe4c5d6f6d85" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/assets/seo/favicons/apple-icon-57x57.png"></link>
+        <link rel="apple-touch-icon" sizes="60x60" href="/assets/seo/favicons/apple-icon-60x60.png"></link>
+        <link rel="apple-touch-icon" sizes="72x72" href="/assets/seo/favicons/apple-icon-72x72.png"></link>
+        <link rel="apple-touch-icon" sizes="76x76" href="/assets/seo/favicons/apple-icon-76x76.png"></link>
+        <link rel="apple-touch-icon" sizes="114x114" href="/assets/seo/favicons/apple-icon-114x114.png"></link>
+        <link rel="apple-touch-icon" sizes="120x120" href="/assets/seo/favicons/apple-icon-120x120.png"></link>
+        <link rel="apple-touch-icon" sizes="144x144" href="/assets/seo/favicons/apple-icon-144x144.png"></link>
+        <link rel="apple-touch-icon" sizes="152x152" href="/assets/seo/favicons/apple-icon-152x152.png"></link>
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/seo/favicons/apple-icon-180x180.png"></link>
+        <link rel="icon" type="image/png" sizes="192x192" href="/assets/seo/favicons/android-icon-192x192.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/seo/favicons/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="96x96" href="/assets/seo/favicons/favicon-96x96.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/seo/favicons/favicon-16x16.png"></link>
+        <link rel="icon" href="/assets/seo/favicons/favicon.ico" />
       </head>
       <body>
         <StyledComponentsRegistry>

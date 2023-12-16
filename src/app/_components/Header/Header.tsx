0ab71e5d2659c16b94ff00afac6e3d78b05ab0cd
cobocho <rlaalsrb1111@naver.com>
2023-12-17 02:36:41 +0900

    feat: 헤더 적용

--- src/app/_components/Header/Header.tsx
+++ src/app/_components/Header/Header.tsx
@@ -2,13 +2,12 @@
 
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import styled from 'styled-components';
 
 import SearchIcon from '@/app/_components/Icons/SearchIcon';
 
 import ThemeToggle from './ThemeToggle/ThemeToggle';
 import ScrollProgressBar from './ScrollProgressBar/ScrollProgressBar';
-import LAYOUT_VARIABLES from '@/styles/layout-variables';
+import { header, headerLink, headerRightSection, headerWrapper, search } from './Header.css';
 
 const Header = () => {
   const [position, setPosition] = useState<number>(0);
@@ -29,148 +28,29 @@ const Header = () => {
   }, [position]);
 
   return (
-    <Container className={`${visible ? '' : 'hide'}${position === 0 ? 'top' : ''}`}>
-      <div className="header-wrapper">
+    <header className={`${headerWrapper} ${visible ? '' : 'hide'}${position === 0 ? 'top' : ''}`}>
+      <div className={header}>
         <div className="header-links">
-          <Link href={'/'} className="main-link">
+          <Link href={'/'} className={`${headerLink} main-link`}>
             cobocho.dev
           </Link>
-          <Link href={'/about'} className="about">
+          <Link href={'/about'} className={`${headerLink} about`}>
             about
           </Link>
-          <Link href={'/tags'} className="tags">
+          <Link href={'/tags'} className={`${headerLink} tags`}>
             tags
           </Link>
         </div>
-        <div className="right-header-section">
+        <div className={headerRightSection}>
           <ThemeToggle />
-          <Link href="/search" className="search">
+          <Link href="/search" className={`${search} search`}>
             <SearchIcon />
           </Link>
         </div>
       </div>
       <ScrollProgressBar />
-    </Container>
+    </header>
   );
 };
-
-const Container = styled.header`
-  display: flex;
-  align-items: center;
-
-  position: sticky;
-  top: 0;
-  left: 0;
-
-  width: 100vw;
-  height: ${LAYOUT_VARIABLES.headerHeight};
-
-  background-color: rgba(0, 0, 0, 0.813);
-
-  z-index: ${LAYOUT_VARIABLES.headerZIndex};
-
-  box-shadow: 0px 4px 10px 5px rgba(0, 0, 0, 0.3);
-  backdrop-filter: blur(10px);
-
-  font-weight: 300;
-  color: #fff;
-
-  transition: all 0.5s;
-
-  &.hide {
-    top: -${LAYOUT_VARIABLES.headerZIndex};
-    box-shadow: none;
-  }
-
-  &.top {
-    box-shadow: none;
-  }
-
-  .header-wrapper {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    position: relative;
-
-    width: 900px;
-    height: 100%;
-
-    margin: 0 auto;
-
-    overflow: hidden;
-
-    .header-links {
-      a {
-        margin-right: 30px;
-
-        font-size: 20px;
-        color: #fff;
-
-        transition: color 0.3s;
-
-        &:hover {
-          color: #acacac;
-        }
-      }
-
-      .main-link {
-        font-size: 32px;
-        margin-right: 30px;
-      }
-    }
-
-    .right-header-section {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      gap: 20px;
-
-      svg:hover {
-        cursor: pointer;
-      }
-
-      .search {
-        position: relative;
-        top: 3px;
-
-        fill: #fff;
-
-        transition: fill 0.3s;
-
-        &:hover {
-          fill: #acacac;
-        }
-      }
-    }
-
-    .scroll-progress-bar {
-      position: absolute;
-      bottom: 0;
-      width: 100%;
-
-      transform-origin: left;
-    }
-  }
-
-  @media (max-width: ${LAYOUT_VARIABLES.breakPoint}) {
-    .header-wrapper {
-      padding: 0 20px;
-
-      .header-links {
-        a {
-          font-size: 16px;
-          margin-right: 10px;
-          transition: transform 0.3s;
-        }
-
-        .main-link {
-          font-size: 24px;
-          margin-right: 15px;
-        }
-      }
-    }
-  }
-`;
 
 export default Header;

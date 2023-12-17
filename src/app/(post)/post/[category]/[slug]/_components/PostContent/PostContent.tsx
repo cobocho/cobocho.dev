82b54d29c1d459b4b12cc2b77ebc23cc00634746
cobocho <rlaalsrb1111@naver.com>
2023-12-17 23:09:04 +0900

    fix: 텍스트 태그 p => span 변경

--- src/app/(post)/post/[category]/[slug]/_components/PostContent/PostContent.tsx
+++ src/app/(post)/post/[category]/[slug]/_components/PostContent/PostContent.tsx
@@ -32,7 +32,7 @@ const PostContent = ({ children, post }: Props) => {
   const customComponent: Partial<SpecialComponents> = useMemo(() => {
     return {
       p({ ...props }) {
-        return <p>{props.children}</p>;
+        return <span>{props.children}</span>;
       },
 
       a({ ...props }) {

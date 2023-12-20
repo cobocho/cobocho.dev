0ab1c61a140531ee28392d5016c702621f1768fb
cobocho <rlaalsrb1111@naver.com>
2023-12-20 17:05:33 +0900

    chore: `next.config`에 `sc` 관련 옵션 제거

--- next.config.js
+++ next.config.js
@@ -6,9 +6,6 @@ const withVanillaExtract = createVanillaExtractPlugin();
 const nextConfig = {
   swcMinify: true,
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
 };
 
 module.exports = withVanillaExtract(nextConfig);

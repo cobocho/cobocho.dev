04eaaec375886774c56cc5a2ab31ffdf18c84d9b
cobocho <rlaalsrb1111@naver.com>
2024-01-16 01:04:27 +0900

    test: Footer 및 Header 스토리 작성

---
+++ src/app/_components/Footer/stories/Footer.stories.tsx
@@ -0,0 +1,25 @@
+import type { StoryObj } from '@storybook/react';
+import Footer from '../Footer';
+import { ToggleRenderer } from '@/lib/test-utils';
+
+export default {
+  title: '공통/푸터',
+
+  component: () => {
+    return <Footer />;
+  },
+};
+
+type Story = StoryObj<typeof Footer>;
+
+export const Default: Story = {
+  name: '라이트 모드',
+};
+
+export const Toggle: Story = {
+  name: '다크 모드',
+
+  render: () => {
+    return ToggleRenderer(Footer);
+  },
+};
---
+++ src/app/_components/Header/stories/Header.stories.tsx
@@ -0,0 +1,37 @@
+import Header from '@/app/_components/Header/Header';
+import { within, userEvent } from '@storybook/testing-library';
+
+import type { StoryObj } from '@storybook/react';
+
+export default {
+  title: '공통/헤더',
+
+  component: () => {
+    return (
+      <div
+        style={{
+          height: '2000px',
+        }}
+      >
+        <Header />
+      </div>
+    );
+  },
+};
+
+type Story = StoryObj<typeof Header>;
+
+export const Default: Story = {
+  name: '라이트 모드',
+};
+
+export const Toggle: Story = {
+  name: '다크 모드',
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const toggleButton = canvas.getByRole('button');
+
+    await userEvent.click(toggleButton);
+  },
+};

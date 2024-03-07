34d44b8a125660de96baa33507fbfb0c0c1b3b24
cobocho <rlaalsrb1111@naver.com>
2024-03-07 18:04:56 +0900

    refactor: 미사용 코드 제거

--- src/app/(postList)/_components/PostList/PostList.css.ts
+++ src/app/(postList)/_components/PostList/PostList.css.ts
@@ -1,58 +1,6 @@
 import { globalStyle, style } from '@vanilla-extract/css';
 
 import LAYOUT_VARIABLES from '@/styles/layout-variables';
-import { Theme } from '@/styles/theme.css';
-
-export const postListTitle = style({
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-});
-
-export const PostListTitleWrapper = style({
-  display: 'flex',
-  alignItems: 'flex-end',
-  height: '62px',
-  marginBottom: '20px',
-  fontWeight: '200',
-
-  '@media': {
-    [`screen and (max-width: ${LAYOUT_VARIABLES.breakPoint}px)`]: {
-      flexDirection: 'column',
-      alignItems: 'flex-start',
-      gap: '5px',
-    },
-  },
-});
-
-export const PostListTitle = style({
-  display: 'block',
-  marginRight: '10px',
-  textTransform: 'uppercase',
-  fontSize: '48px',
-  lineHeight: '1.1',
-
-  '@media': {
-    [`screen and (max-width: ${LAYOUT_VARIABLES.breakPoint}px)`]: {
-      fontSize: '36px',
-    },
-  },
-});
-
-export const PostListDescription = style({
-  position: 'relative',
-  bottom: '6px',
-  color: Theme.secondary,
-  fontSize: '24px',
-  fontWeight: '100',
-
-  '@media': {
-    [`screen and (max-width: ${LAYOUT_VARIABLES.breakPoint}px)`]: {
-      bottom: '0',
-      fontSize: '20px',
-    },
-  },
-});
 
 export const postList = style({
   display: 'grid',

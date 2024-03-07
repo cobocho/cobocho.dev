97bfa6e9754bf85630bf30ee2449bebf497cb662
cobocho <rlaalsrb1111@naver.com>
2024-03-08 03:58:04 +0900

    chore: nextjs 관련 jest config 추가

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,10 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
 
 const config: Config = {
   clearMocks: true,
@@ -12,4 +13,4 @@ const config: Config = {
   testEnvironment: 'jsdom',
 };
 
-export default config;
+export default createJestConfig(config);

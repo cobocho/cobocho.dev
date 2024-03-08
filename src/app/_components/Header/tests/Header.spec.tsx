3437b71db0fd89ee4f0ee787b0fce261acec037e
Kim Min-gyu <99083803+cobocho@users.noreply.github.com>
2024-03-09 04:36:37 +0900

    Yarn => pnpm & 테스트 환경 설정 및 일부 작성 (#36)
    * chore: 테스팅 관련 패키지 설치
    
    * chore: nextjs 관련 jest config 추가
    
    * test: 유틸리티 함수 테스트코드 작성
    
    * test: footer 및 header 테스트 작성
    
    * test: `useModal` 커스텀 훅 테스트 작성
    
    * chore: `console.log` 제거
    
    * refactor: 컨텍스트 외부 호출 시 에러 반환
    
    * chore: yarn => pnpm

---
+++ src/app/_components/Header/tests/Header.spec.tsx
@@ -0,0 +1,37 @@
+import { screen, within } from '@testing-library/react';
+
+import { renderer } from '../../../../lib/test-utils';
+import Header from '../Header';
+
+describe('Header 테스트', () => {
+  it('다크모드 토글 버튼을 렌더링한다.', async () => {
+    await renderer(<Header posts={[]} />);
+    const darkmodeButton = screen.getByTestId('dark-mode');
+
+    expect(darkmodeButton).toBeInTheDocument();
+  });
+
+  it('다크모드 토글 버튼을 클릭시 다크 모드가 변경된다.', async () => {
+    const { user } = await renderer(<Header posts={[]} />);
+    const darkmodeButton = screen.getByTestId('dark-mode');
+
+    expect(darkmodeButton).toHaveValue('light');
+
+    await user.click(darkmodeButton);
+
+    expect(darkmodeButton).toHaveValue('dark');
+  });
+
+  it('검색 버튼을 클릭시 검색 모달이 열린다.', async () => {
+    const { user } = await renderer(<Header posts={[]} />);
+    const searchButton = screen.getByTestId('search');
+
+    await user.click(searchButton);
+
+    const searchModal = within(document.getElementById('modal') as HTMLDivElement).getByText(
+      '검색',
+    );
+
+    expect(searchModal).toBeInTheDocument();
+  });
+});
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,53 +3,6 @@ export function replaceSpaceToHyphen(str: unknown) {
     return str.split(' ').join('-');
   }
 }
-
-export const timeAgo = (date: string) => {
-  const postedDate = Number(new Date(date));
-  const today = Number(new Date());
-  const diff = today - postedDate;
-
-  const seconds = Math.floor(diff / 1000);
-
-  let interval = Math.floor(seconds / (60 * 60 * 24 * 30 * 12));
-  if (interval > 1) {
-    return interval + ' years ago';
-  }
-
-  interval = Math.floor(seconds / (60 * 60 * 24 * 30));
-  if (interval >= 1) {
-    return interval + ' months ago';
-  }
-
-  interval = Math.floor(seconds / (60 * 60 * 24));
-  if (interval > 1) {
-    return interval + ' days ago';
-  }
-
-  interval = Math.floor(seconds / (60 * 60));
-  if (interval > 1) {
-    return interval + ' hours ago';
-  }
-
-  interval = Math.floor(seconds / 60);
-  if (interval > 1) {
-    return interval + ' minutes ago';
-  }
-
-  if (seconds < 10) return 'just now';
-
-  return Math.floor(seconds) + ' seconds ago';
-};
-
-export const sortDescending = <T>(list: T[], prop: keyof T) => {
-  return list.sort((a, b) => {
-    if (typeof a[prop] !== 'number' || typeof b[prop] !== 'number') {
-      throw new Error('property that cannot be arithmetic');
-    }
-    return (b[prop] as number) - (a[prop] as number);
-  });
-};
-
 export function getMinRead(text: string): number {
   const words = text.trim().split(/\s+/);
   const wordCount = words.length;

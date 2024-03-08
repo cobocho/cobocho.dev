044262013d826d22891ab6b30400000ebe707e0f
cobocho <rlaalsrb1111@naver.com>
2024-03-09 04:50:35 +0900

    fix: 제거된 유틸리티 복구

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,6 +10,15 @@ export function getMinRead(text: string): number {
   return minRead;
 }
 
+export const sortDescending = <T>(list: T[], prop: keyof T) => {
+  return list.sort((a, b) => {
+    if (typeof a[prop] !== 'number' || typeof b[prop] !== 'number') {
+      throw new Error('property that cannot be arithmetic');
+    }
+    return (b[prop] as number) - (a[prop] as number);
+  });
+};
+
 export const calculatePages = (postQuantity: number) => {
   const pages = Array.from({ length: Math.ceil(postQuantity / 10) }, (_, i) => i + 1);
   return pages;

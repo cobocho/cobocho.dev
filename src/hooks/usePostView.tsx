c76ce546da8831c7467b2db1dbdffdb8cab30146
cobocho <rlaalsrb1111@naver.com>
2024-03-22 22:42:11 +0900

    test: `usePostView` 테스트 작성

--- src/hooks/usePostView.tsx
+++ src/hooks/usePostView.tsx
@@ -16,7 +16,7 @@ export const postViewFlag = {
 
 export type PostViewFlag = (typeof postViewFlag)[keyof typeof postViewFlag];
 
-const PostView = createContext<ViewContextValues>({ postView: 1 } as ViewContextValues);
+const PostView = createContext<ViewContextValues | undefined>(undefined);
 
 interface Props {
   children: React.ReactNode;
@@ -58,5 +58,11 @@ export const PostViewProvider = ({ children }: Props) => {
 };
 
 export const usePostView = () => {
-  return useContext(PostView);
+  const context = useContext(PostView);
+
+  if (!context) {
+    throw new Error('usePostView 커스텀 훅은 PostViewProvider 내부에서 호출해야합니다.');
+  }
+
+  return context;
 };
